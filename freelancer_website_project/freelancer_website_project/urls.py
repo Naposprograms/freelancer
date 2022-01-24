8404a2b8f85156de270ce259e04b76aeb25aaebc
Napo <gabrielnapolitano@hotmail.com>
2022-01-23 22:18:52 -0300

    Cleaned gitignored files

--- freelancer_website_project/freelancer_website_project/urls.py
+++ freelancer_website_project/freelancer_website_project/urls.py
@@ -18,8 +18,14 @@ from django.urls import path
 
 from helloworld import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
